1bdccf1e952d61d342e2cd68488f54b7e9fa8b92
rjohnson1019 <johnson.ryan1019@gmail.com>
2022-01-19 12:26:50 -0700

    added soccer data; get team names for each sport

--- fivethirtyeight_data.R
+++ fivethirtyeight_data.R
@@ -2,8 +2,8 @@
 # Web Scraping Script for Sports Data
 ##############################
 
+#### Pull in newest data ####
 fivethirtyeight_dir <- "C:/Users/johns/Documents/R/fivethirtyeight/data"
-
 
 # NHL Forecasts
 nhl_url <- paste0(fivethirtyeight_dir, "/nhl-forecasts/README.md") %>%
@@ -22,3 +22,20 @@ nfl_url <- paste0(fivethirtyeight_dir, "/nfl-elo/README.md") %>%
    readr::read_file() %>%
    str_extract("https://.+csv")
 nfl_data <- fread(nfl_url)
+
+# Soccer Forecasts
+soccer_url <- paste0(fivethirtyeight_dir, "/soccer-spi/README.md") %>%
+   readr::read_file() %>%
+   str_extract("https://.+csv")
+soccer_data <- fread(soccer_url)
+
+
+#### Get Teams/Leagues ####
+nhl_teams <- nhl_data[,unique(home_team)]
+nba_teams <- nba_data[,unique(team1)]
+nfl_data <- nfl_data[,unique(team1)]
+soccer_names <- soccer_data[,unique(team1)]
+soccer_leagues <- soccer_data[,unique(league)]
+
+
+#### Dates ####
