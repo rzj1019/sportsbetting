cc1466cd1d89fe3fc363dc4f1dcfbf590f233746
rjohnson1019 <johnson.ryan1019@gmail.com>
2022-01-19 12:27:55 -0700

    start to build out bet entry form and data viewing

--- sports_betting_app/app.R
+++ sports_betting_app/app.R
@@ -5,24 +5,41 @@ library(shiny)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-   # Display PointSpread Table
-   selectInput("dataset", label = "Types of Bets", choices = PointSpread),
-   verbatimTextOutput("summary"),
-   tableOutput("table")
+   # Bet entry for point spread entries
+   dateInput("gameday", "Game Day (yyyy/mm/dd)"),
+   textInput("sport", "Sport"),
+   textInput("league", "League"), # make multiple=True once code has list of leagues
+   textInput("team", "Team"), # make multiple=True once code has list of teams
+   textInput("bettype", "Bet Type"), # make multiple=True once code has list of bettypes
+   textInput("overunderval", "Over/Under Value"),
+   textInput("draftkingsodds", "DraftKings Odds", value = 0),
+   textInput("538odds", "FiveThiryEight Odds", value = 0),
+   textInput("payoutodds", "Payout Odds", value = 0),
+   textInput("wager", "Wager", value = 00.00),
+   actionButton("savebet", "Save Bet"),
+   # Insert data input header for "Results" Section
+   textInput("actualpayout", "Actual Payout", value = 0),
+   textInput("gameresult", "Game Results"),
+   actionButton("saveresults", "Save Results")
 
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
+   # Reactive Expression
+      betpage <- reactive({
+         get(input$Bets, "package:datasets")
+   })
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+   output$summary <-  renderPrint({
+      # Using reactive expression
+      summary(betpage())
+   })
+   output$table <- renderTable({
+      betpage()
+   })
+
 }
 
 # Run the application
