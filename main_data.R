b3af44b7efbb17d6a805b6e017114e53329a4d06
rjohnson1019 <johnson.ryan1019@gmail.com>
2022-01-19 12:27:28 -0700

    get dats for which bets exist

--- main_data.R
+++ main_data.R
@@ -3,12 +3,22 @@
 # https://docs.google.com/spreadsheets/d/16RhAnUcACgKTYaJZKT7JzWDd57zcFNzMkXL29KGo9rg/edit#gid=2081509076
 ###################################
 
-# Authentication
+# --- Pull in Google Sheets Data ---
+# authorize account
 googlesheets4::gs4_auth(email = TRUE)
 
-# Bet538 sheet ids
+# Bet538 sheets
 bet538_id <- "16RhAnUcACgKTYaJZKT7JzWDd57zcFNzMkXL29KGo9rg"
 PointSpread <- read_sheet(ss = bet538_id, sheet = "PointSpread") %>% data.table()
 Moneyline <- read_sheet(ss = bet538_id, sheet = "Moneyline") %>% data.table()
 Parlays <- read_sheet(ss = bet538_id, sheet = "Parlays") %>% data.table()
 
+
+# --- Get gameday dates ---
+ps_dates <- PointSpread[,unique(gameday)] %>% as.Date()
+ml_dates <- Moneyline[,unique(gameday)] %>% as.Date()
+par_dates <- Parlays[,unique(gameday)] %>% as.Date()
+
+bet_dates <- c(ps_dates, ml_dates, par_dates) %>%
+   unique() %>%
+   sort()
