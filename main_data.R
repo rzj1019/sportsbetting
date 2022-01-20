b520cda70bdc3ee21c5ebb8da523455d026974dd
rjohnson1019 <johnson.ryan1019@gmail.com>
2022-01-19 21:07:01 -0700

    change variables to use . instead of _

--- main_data.R
+++ main_data.R
@@ -8,17 +8,17 @@
 googlesheets4::gs4_auth(email = TRUE)
 
 # Bet538 sheets
-bet538_id <- "16RhAnUcACgKTYaJZKT7JzWDd57zcFNzMkXL29KGo9rg"
-PointSpread <- read_sheet(ss = bet538_id, sheet = "PointSpread") %>% data.table()
-Moneyline <- read_sheet(ss = bet538_id, sheet = "Moneyline") %>% data.table()
-Parlays <- read_sheet(ss = bet538_id, sheet = "Parlays") %>% data.table()
+bet538.id <- "16RhAnUcACgKTYaJZKT7JzWDd57zcFNzMkXL29KGo9rg"
+PointSpread <- read_sheet(ss = bet538.id, sheet = "PointSpread") %>% data.table()
+Moneyline <- read_sheet(ss = bet538.id, sheet = "Moneyline") %>% data.table()
+Parlays <- read_sheet(ss = bet538.id, sheet = "Parlays") %>% data.table()
 
 
 # --- Get gameday dates ---
-ps_dates <- PointSpread[,unique(gameday)] %>% as.Date()
-ml_dates <- Moneyline[,unique(gameday)] %>% as.Date()
-par_dates <- Parlays[,unique(gameday)] %>% as.Date()
+ps.dates <- PointSpread[,unique(gameday)] %>% as.Date()
+ml.dates <- Moneyline[,unique(gameday)] %>% as.Date()
+par.dates <- Parlays[,unique(gameday)] %>% as.Date()
 
-bet_dates <- c(ps_dates, ml_dates, par_dates) %>%
+bet_dates <- c(ps.dates, ml.dates, par.dates) %>%
    unique() %>%
    sort()
