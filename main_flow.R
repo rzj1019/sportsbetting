9159fa235353364a542dc48003fa012bc02155c5
rjohnson1019 <johnson.ryan1019@gmail.com>
2022-01-21 22:24:07 -0700

    fixed source file name

--- main_flow.R
+++ main_flow.R
@@ -17,7 +17,7 @@ dir <- getwd()
 source("system_init.R")
 
 # Pull in most up to date FiveThirtyEight data
-source("fivethirtyeight_data.R")
+source("fivethirtyeight.R")
 
 # Pull in Bet538 data
 source("main_data.R")
