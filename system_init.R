5c92033f4f3c6de7f84c0792e7f5ce04aed8dc28
rjohnson1019 <johnson.ryan1019@gmail.com>
2022-01-21 20:11:23 -0700

    added namediff function

--- system_init.R
+++ system_init.R
@@ -2,7 +2,7 @@
 # Environment loader
 ###########################
 
-# check working directories and set if needed
+#### check working directories and set if needed ####
 if(getwd()==paste0("C:/Users/",Sys.info()[6],"/Documents/R/sportsbetting")){
    message(paste0("Current working directory: ", getwd()))
 } else{
@@ -10,7 +10,7 @@ if(getwd()==paste0("C:/Users/",Sys.info()[6],"/Documents/R/sportsbetting")){
    message(paste0("Directory changed, set to: ", getwd()))
 }
 
-# Loading Needed packages
+##### Loading Needed packages ####
 
 # Checks for needed package [pacman] and then loads other packages [pkgs]
 if(!require("pacman", character.only = T)){
@@ -39,3 +39,17 @@ message("Directories changed and loaded packages:
       rvest
       shiny
       tidyverse")
+
+#### Helpful Functions ####
+namediff <- function(x, y){
+   message("Columns in ", deparse(substitute(x)), " that are not in ", deparse(substitute(y)), ":")
+   xdiff <- setdiff(names(x), names(y))
+   for (x in 1:length(xdiff)) {
+      cat(xdiff[x], "\n")
+   }
+   message("Columns ", deparse(substitute(y)), " that are not in ", deparse(substitute(x)), ":")
+   ydiff <- setdiff(names(y), names(x))
+   for (y in 1:length(ydiff)) {
+      cat(ydiff[x], "\n")
+   }
+}
